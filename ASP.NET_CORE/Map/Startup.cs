6da1869f6382b189f6769ffe72c14e9c66990349
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-03-10 17:52:25 +0400

    Map

---
+++ ASP.NET_CORE/Map/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Map
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Map("/home", (z) =>
+            {
+                z.Map("/second", (appSecond) =>
+                           {
+                               appSecond.Run(async (context) =>
+                               await context.Response.WriteAsync("home Map"));
+                           });
+
+            });
+
+
+            app.Map("/index", (index) =>
+            {
+                index.Run(async (context) =>
+                await context.Response.WriteAsync("<h1>index Map</h1>"));
+            });
+
+            app.Run(async (context) =>
+            {
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
