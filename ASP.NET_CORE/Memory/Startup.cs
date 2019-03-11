f6265e1374d10135e1678d448080b92159b70482
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-03-11 17:20:50 +0400

    Memory

---
+++ ASP.NET_CORE/Memory/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Memory
+{
+    public class Startup
+    {
+        public IConfiguration AppConfiguration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddInMemoryCollection(new Dictionary<string, string>
+            { { "Color", "Green"},
+            { "Content", "Any Text"}
+            });
+            AppConfiguration = builder.Build();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+{
+    await context.Response.WriteAsync($"<p style='color: {AppConfiguration["color"]};'> {AppConfiguration["Content"]}</p>");
+});
+        }
+    }
+}
