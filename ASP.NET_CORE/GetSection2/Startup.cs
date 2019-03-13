9676a822fdd2cafc442236043bdad2e200ebfff6
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-03-13 15:44:34 +0400

    GetSection2

---
+++ ASP.NET_CORE/GetSection2/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GetSection2
+{
+    public class Startup
+    {
+        public IConfiguration AppConfiguration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(env.ContentRootPath);
+
+            builder.AddJsonFile("json.json");
+
+            AppConfiguration = builder.Build();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            var sections = AppConfiguration.GetSection("Users");
+            var sectionUsers = sections.GetChildren();
+
+
+            app.Run(async (context) =>
+            {
+                foreach (var item in sectionUsers)
+                {
+                    await context.Response.WriteAsync($"<br>Company: {item.Key}:{item.GetSection("Number").Value}</br>");
+                }
+
+            });
+        }
+    }
+}
