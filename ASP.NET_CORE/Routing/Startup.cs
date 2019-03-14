608a7a5c48edbf13f639cb20b19c874b3f012dad
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-03-14 20:12:51 +0400

    Routing

---
+++ ASP.NET_CORE/Routing/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Specialized;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Routing
+{
+    public class Startup
+    {
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var routebuilder = new RouteBuilder(app);
+
+            routebuilder.MapRoute("/Home",
+                async (context) =>
+                {
+                    await context.Response.WriteAsync("Home Page");
+                });
+
+            routebuilder.MapRoute("/Home/Courses",
+               
+               async (context) =>
+               {
+                   StringCollection courses = new StringCollection()
+                   {
+                       "C# courses",
+                       "JS Courses",
+                       "Java Cources",
+                       "HTML CSS Cources"
+                   };
+
+                   await context.Response.WriteAsync("here of the list of avaliable courses");
+                   foreach (var item in courses)
+                   {
+                       await context.Response.WriteAsync($"<br>{item}</br>");
+                   }
+               });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Default Page!");
+            });
+        }
+    }
+}
