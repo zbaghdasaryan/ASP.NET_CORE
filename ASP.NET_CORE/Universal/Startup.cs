a893b4a5b863fa6f74081f1bd17ad7a8444e018b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-03-14 20:25:18 +0400

    Universal

---
+++ ASP.NET_CORE/Universal/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Specialized;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Universal
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var routebuilder = new RouteBuilder(app);
+
+            routebuilder.MapRoute("{controler}",
+                async (context) =>
+                {
+                    await context.Response.WriteAsync("{controler} template use");
+                });
+
+            routebuilder.MapRoute("{controler}/{action}",
+
+               async (context) =>
+               {
+                   await context.Response.WriteAsync("{controler}/{action} template use");
+               });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Default Page!");
+            });
+        }
+    }
+}
