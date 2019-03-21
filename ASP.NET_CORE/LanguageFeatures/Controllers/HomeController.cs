a4c3e067452ce62e5a4aa88463a5b6e01259c956
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-03-21 18:14:15 +0400

    LanguageFeatures

---
+++ ASP.NET_CORE/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> result = new List<string>();
+            foreach (var p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                result.Add(string.Format("Name: {0},  Price: {1}", name, price));
+            }
+            // return View(new string[] { "C#", "Language", "Features" });
+            return View(result);
+        }
+    }
+}
