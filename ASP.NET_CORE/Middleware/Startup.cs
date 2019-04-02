9291ff9dcd455a678fe23ea4e973eeac68c8e46c
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-04-02 23:25:45 +0400

    Middleware

---
+++ ASP.NET_CORE/Middleware/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Middleware
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<TokenMiddleware>();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
---
+++ ASP.NET_CORE/Middleware/TokenMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Middleware
+{
+    public class TokenMiddleware
+    {
+        public readonly RequestDelegate _next;
+        public TokenMiddleware(RequestDelegate _next)
+        {
+            this._next = _next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (token != "12345678")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
+
