448607c5f0370de3f55ba983ef4146138cd70243
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-04-06 13:36:26 +0400

    SetOfFields

--- ASP.NET_CORE/SetOfFields/Controllers/HomeController.cs
+++ ASP.NET_CORE/SetOfFields/Controllers/HomeController.cs
@@ -15,14 +15,18 @@ namespace SetOfFields.Controllers
             return View();
         }
 
-        public string ProceedResult(string name, string surname, int age)
-        {
-            User user = new User()
-            {
-                Name = name,
-                Surname = surname,
-                Age = age
-            };
+        //public string ProceedResult(string name, string surname, int age)
+        //{
+        //    User user = new User()
+        //    {
+        //        Name = name,
+        //        Surname = surname,
+        //        Age = age
+        //    };
+        //    return $"Name: {user.Name}. Surname: {user.Surname}. Age: {user.Age}";
+        //}
+        public string ProceedResult(User user)
+        {           
             return $"Name: {user.Name}. Surname: {user.Surname}. Age: {user.Age}";
         }
 
