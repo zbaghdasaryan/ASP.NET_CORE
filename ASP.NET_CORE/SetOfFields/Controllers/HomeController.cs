710ca580319f9f099a8681c119b0d9703528d639
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-04-06 13:25:16 +0400

    SetOfFields

---
+++ ASP.NET_CORE/SetOfFields/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SetOfFields.Models;
+
+namespace SetOfFields.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string ProceedResult(string name, string surname, int age)
+        {
+            User user = new User()
+            {
+                Name = name,
+                Surname = surname,
+                Age = age
+            };
+            return $"Name: {user.Name}. Surname: {user.Surname}. Age: {user.Age}";
+        }
+
+    }
+}
