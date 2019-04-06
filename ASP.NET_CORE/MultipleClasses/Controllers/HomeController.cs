625206e05bac45d519441e179fe11f151b2ed1cb
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-04-06 13:50:41 +0400

    MultipleClasses

---
+++ ASP.NET_CORE/MultipleClasses/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultipleClasses.Models;
+
+namespace MultipleClasses.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string ProceedResult(User[] users)
+        {
+            string result = "";
+            foreach (User user in users)
+                result += $"Name: {user.Name}. Surname: {user.Surname}. Age: {user.Age}.\n";
+
+            return result;
+
+        }
+    }
+}
