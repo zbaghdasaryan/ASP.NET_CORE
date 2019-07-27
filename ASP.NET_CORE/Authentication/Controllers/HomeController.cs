4c047eb9b6d6c5ef54c051bd1c0e017fe3805a35
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-07-28 00:27:14 +0400

    Authentication

--- ASP.NET_CORE/Authentication/Controllers/HomeController.cs
+++ ASP.NET_CORE/Authentication/Controllers/HomeController.cs
@@ -1,29 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Authentication.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
-namespace Authentication.Controllers
+namespace AuthApp.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Content(User.Identity.Name);
         }
     }
 }
