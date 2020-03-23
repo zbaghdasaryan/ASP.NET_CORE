71393c677d15381429f0640f15674cee53b72342
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-03-23 15:07:52 +0400

    add RoleApp

---
+++ ASP.NET_CORE/RolesApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RolesApp.Models;
+
+namespace RolesApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Authorize(Roles = "admin, user")]
+        public IActionResult Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultNameClaimType).Value;
+            return Content($"ваша роль: {role}");
+        }
+        
+        [Authorize(Roles = "admin")]
+        public IActionResult About()
+        {
+            return Content("Вход только для администратора");
+        }
+    }
+}
---
+++ ASP.NET_CORE/RolesApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RolesApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public int? RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+        public Role()
+        {
+            Users = new List<User>();
+        }
+    }
+}
